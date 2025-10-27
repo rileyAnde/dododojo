99e7abafc696c74ead634d31477626f766e37df8
hrsmith1 <hannahrsmith002@gmail.com>
2025-10-26 20:23:17 -0500

    user services CRUD operations and tests

---
+++ src/routes/user_services-routes.ts
@@ -0,0 +1,13 @@
+//routes for user services
+import { getUserServices, addUserService, updateUserService, deleteUserService } from '../controllers/user_services-controller';
+const express = require('express')
+const user_services = express.Router()
+
+// authenticate user service routes
+user_services.get('/user/:username', getUserServices);
+user_services.post('/user/:username', addUserService);
+user_services.put('/user/:userId', updateUserService);
+user_services.delete('/user/:userId', deleteUserService);
+
+export default user_services;
+
