3e14bacb0fe8997fbef5811ec3dec71a6d472707
hrsmith1 <hannahrsmith002@gmail.com>
2025-11-03 09:15:58 -0600

    update models

--- src/models/accounts.ts
+++ src/models/accounts.ts
@@ -1,9 +1,13 @@
 //file for models that can be passed between frontend and backend
 
 export interface Account {
-    id: string;
+    id: Int32Array;
     username: string;
     passwordHash: string;
+    level: Int32Array;
+    inventory: Map<string, number>;
+    primaryDeck: Deck;
+    gymsOwned: string[];
     createdAt: Date;
     updatedAt: Date;
     deck: Deck;
